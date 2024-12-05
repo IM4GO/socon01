a768f1d45d9ecbba4ef4c999e1ede20bf130bbe9
IM4GO <kimq1044@naver.com>
2024-12-05 12:41:49 +0900

    test_upload3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from "react";
-import Potato from "./Potato";
+
+function Food({name, picture}) {
+  return (
+    <div>
+      <h1>I like {name}</h1>
+      <img src={picture}/>
+    </div>
+  );
+}
+
+const foodILike = [
+  {
+    name:'Kimchi',
+    image:'https://cicili.tv/wp-content/uploads/2022/07/Traditional-Kimchi-1-scaled.jpeg',
+  },
+  {
+    name:'Samgyeopsal',
+    image:'https://www.wandercooks.com/wp-content/uploads/2024/09/korean-bbq-pork-belly-5-480x270.jpg',
+  }
+];
 
 function App() {
   return(
     <div>
-      <h1>hello</h1>
-      <Potato />
+      {foodILike.map(dish => (
+        <Food name={dish.name} picture={dish.image}/>
+      ))}
     </div>
   );
 }
